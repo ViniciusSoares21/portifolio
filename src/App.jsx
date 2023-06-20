d872f27e8fc6aefee1b455a5b71509504cd9acd7
Vinicius Soares <viniciussoares72943@gmail.com>
2023-06-20 10:59:01 -0300

    feat: adiciona pagina NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Home from './pages/Home'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 function App() {
 
   return (
     <main>
       <Routes>
-        <Route path='/' element={ <Home /> }/>
-        <Route path='/About' element={ <About />}/>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/About' element={ <About />} />
+        <Route path='*' element={ <NotFound />} />
       </Routes>
     </main>
   )
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NotFound() {
+  return (
+    <section>
+      <Link to="/">
+        <h3>Volatar para HOME</h3>
+      </Link>
+    </section>
+  )
+}
+
+export default NotFound
