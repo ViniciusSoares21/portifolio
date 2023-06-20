9c0dd3d910e41b69b4ce25e6b528ec38f46d684c
Vinicius Soares <viniciussoares72943@gmail.com>
2023-06-20 10:49:09 -0300

    feat: adiciona pagina About

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from './pages/Home'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <main>
       <Routes>
         <Route path='/' element={ <Home /> }/>
+        <Route path='/About' element={ <About />}/>
       </Routes>
     </main>
   )
