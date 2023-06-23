ac577c7f9df5644ba3fad1ae406c3bb92229fb4e
Vinicius Soares <viniciussoares72943@gmail.com>
2023-06-23 11:43:56 -0300

    feat: adiciona os projetos a pagina projectsDetils, de forma generica e c=icicia o css

--- src/assets/database/prejects.js
+++ src/assets/database/prejects.js
@@ -2,8 +2,8 @@ const projects = [
   {
     image: 'src/assets/database/image/Rectangle 2.png',
     title: 'Trybe futebol clube',
-    shortDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley ",
-    description: 'NOT IMPLEMETION',
+    shortDescription: "Lorem Ipsum is simply dumy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley ",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ebr Lorem Ipsum has been the industry's standard dummy text ever since the cfe 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     technologies: [
       'src/assets/database/imageTechnology/react.svg', 
       'src/assets/database/imageTechnology/jest.svg', 
--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-import styles from './ProjectDetails.module.css'
+import styles from './ProjectDetails.module.css';
+import projects from '../assets/database/prejects';
 
 
 function ProjectDetails() {
@@ -14,7 +15,36 @@ function ProjectDetails() {
   return (
     <main>
       <NavBar />
-      <h1 className={styles.title}>{titleProject}</h1>
+      <section className={styles.container}>
+        <h1 className={styles.title}>{titleProject}</h1>
+        {projects.filter((item) => item.title === titleProject)
+          .map(({title, description, image, technologies, links}) =>
+            <article key={title} className={styles.subContainer}>
+              <img src={image} alt=""/>
+              <h3 
+                style={{ textAlign: 'center', marginBottom: '20px'}}
+              >
+                DESCRIÇÃO DO PROJETO
+              </h3>
+              <p style={{ textAlign: 'center', marginBottom: '30px' }}>
+                {description}
+              </p>
+              <h3>LINGUAGEM E FERRAMENTAS</h3>
+              <div className={styles.containerTechnologies}>
+                {technologies.map((item) => 
+                  <img key={item} src={item} alt="tecnoligia" />
+                )}
+              </div>
+              <h3>LINKS</h3>
+              <div className={styles.containerLinks}>
+                {links.map((item) => 
+                    <a key={item.link + item.name} href={item.link}>{item.name}</a>
+                )}
+              </div>
+            </article>
+          
+          )}
+      </section>
       <Footer />
     </main>
   )
