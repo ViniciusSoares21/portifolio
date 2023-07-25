fb072c893a13156efe989cb10cd43feb6d021773
Vinicius Soares <viniciussoares72943@gmail.com>
2023-07-25 07:58:22 -0300

    feat: arruma todas imagens dos projetos  para aparecer no servidor

--- src/assets/database/prejects.js
+++ src/assets/database/prejects.js
@@ -16,7 +16,16 @@ import pytest from '../database/imageTechnology/pytest.svg';
 import flask from '../database/imageTechnology/flesk.svg';
 import beautifulsoup from '../database/imageTechnology/-beautifulsoup-black.svg';
 import pandas from '../database/imageTechnology/pandas.svg';
-import imggcontrole from '../database/image/gcontrole.png'
+
+// Imgens dos projetos
+import blogApi from '../database/image/BlogAPI.png';
+import TrybersAndDragons from '../database/image/trybeAndDragons.png';
+import CarShop from '../database/image/CarShop.png';
+import JobInsights from '../database/image/Job.png';
+import InventoryReport from '../database/image/inventory-report.png';
+import TechNews from '../database/image/tech-news.png';
+import RestaurantOrders from '../database/image/restaurant-orders.png';
+import imggcontrole from '../database/image/gcontrole.png';
 
 
 const projects = [
@@ -39,7 +48,7 @@ const projects = [
     showOnHome: true
   },
   {
-    image: 'src/assets/database/image/BlogAPI.png',
+    image: blogApi,
     title: 'Blogs API',
     shortDescription: "Esse projeto foi desenvolvido para a criação de uma API com o banco de dados Mysql, estruturado a partir dos princios de POO e SOLID, destinados para a criação de um blog.",
     description: "Esse projeto foi desenvolvido para a criação de uma API com o banco de dados Mysql, estruturado a partir dos princios POO e SOLID, destinados para a criação de um blog. Durante o desenvolvimento, foi criada uma aplicação em Node.js utilizando o pacote sequelize para implementar as funcionalidades de criação, (CRUD) de posts, e login. Foram criados endpoints que estão conectados ao banco de dados, seguindo os princípios do REST. Para criar um post, é preciso fornecer dados de usuário e realizar o login, o que reforça a conexão entre usuário e post. Além disso, foram usadas categorias para classificar os posts, estabelecendo ligações entre posts e categorias, assim como entre categorias e posts.",
@@ -57,7 +66,7 @@ const projects = [
     showOnHome: false
   },
   {
-    image: 'src/assets/database/image/trybeAndDragons.png',
+    image: TrybersAndDragons,
     title: 'Trybers and dragons',
     shortDescription: "Neste projeto, apliquei os princípios da arquitetura SOLID e da Programação Orientada a Objetos (POO) em uma estrutura de jogos de interpretação de papéis, conhecidos como RPG (Role Playing Game).",
     description: "Neste projeto, apliquei os princípios da arquitetura SOLID e da Programação Orientada a Objetos (POO) em uma estrutura de jogos de interpretação de papéis, conhecidos como RPG (Role Playing Game).",
@@ -75,7 +84,7 @@ const projects = [
     showOnHome: false
   },
   {
-    image: 'src/assets/database/image/CarShop.png',
+    image: CarShop,
     title: 'Car Shop',
     shortDescription: "Neste projeto, apliquei os princípios de Programação Orientada a Objetos (POO) para construir uma API com operações CRUD para gerenciar uma concessionária de veículos. Utilizei o banco de dados MongoDB e o framework Mongoose para essa finalidade.",
     description: "Neste projeto, apliquei os princípios de Programação Orientada a Objetos (POO) para construir uma API com operações CRUD para gerenciar uma concessionária de veículos. Utilizei o banco de dados MongoDB e o framework Mongoose para essa finalidade.",
@@ -112,7 +121,7 @@ const projects = [
     showOnHome: true
   },
   {
-    image: 'src/assets/database/image/Job.png',
+    image: JobInsights,
     title: 'Job insights',
     shortDescription: "Neste projeto, implementei análises de um conjunto de dados sobre empregos.",
     description: "Neste projeto, implementei análises de um conjunto de dados sobre empregos. Escrevi testes com Pytest para garantir a qualidade das análises. Além disso, enfrentei o desafio de criar uma rota e view para um novo recurso no Flask. Durante o projeto, aprimorei habilidades em Python, estruturas condicionais, repetições, manipulação de arquivos, funções, tratamento de exceções e importação de módulos personalizados.",
@@ -130,7 +139,7 @@ const projects = [
     showOnHome: false
   },
   {
-    image: 'src/assets/database/image/inventory-report.png',
+    image: InventoryReport,
     title: 'Inventory report',
     shortDescription: "Neste projeto, implementaremos um gerador de relatórios que receberá dados de estoque de arquivos CSV, JSON ou XML como entrada e produzirá relatórios em duas versões: simples e completa.",
     description: "Neste projeto, implementaremos um gerador de relatórios utilizando Programação Orientada a Objetos. O gerador receberá dados de estoque de arquivos CSV, JSON ou XML como entrada e produzirá relatórios em duas versões: simples e completa. O objetivo é criar um gerador flexível, capaz de processar diferentes formatos de arquivo e gerar relatórios detalhados ou resumidos. Com código organizado e eficiente, o projeto atenderá às diversas necessidades dos usuários na análise de dados do estoque.",
@@ -148,7 +157,7 @@ const projects = [
     showOnHome: false
   },
   {
-    image: 'src/assets/database/image/tech-news.png',
+    image: TechNews,
     title: 'Tech news',
     shortDescription: "Neste projeto, o objetivo principal é fazer consultas em notícias sobre tecnologia. As notícias serão obtidas por meio da raspagem do blog da Trybe.",
     description: "Neste projeto, o objetivo principal é fazer consultas em notícias sobre tecnologia. As notícias serão obtidas por meio da raspagem do blog da Trybe. As principais habilidades trabalhadas incluem o uso do terminal interativo do Python, a escrita e importação de módulos personalizados, a aplicação de técnicas de raspagem de dados para extrair informações de conteúdo HTML e o armazenamento dos dados coletados em um banco de dados. Com essas etapas, o projeto permitirá realizar consultas e análises em notícias relevantes sobre tecnologia, obtidas através da raspagem do blog da Trybe.",
@@ -166,7 +175,7 @@ const projects = [
     showOnHome: false
   },
   {
-    image: 'src/assets/database/image/restaurant-orders.png',
+    image: RestaurantOrders,
     title: 'Restaurant orders',
     shortDescription: "Neste projeto, finalizarei uma ferramenta de construção de cardápios para o restaurante",
     description: "Neste projeto, finalizarei uma ferramenta de construção de cardápios para o restaurante 'Spaghetti Shrimp Chapa Quente Curry'. Serão realizados testes para as classes existentes e desenvolvida uma nova classe para mapear pratos e suas receitas com ingredientes e quantidades. Também criarei classes para gerar os cardápios para os clientes e gerenciar o estoque de ingredientes. Praticarei o uso de Hashmaps com Dict e Set do Python, a ferramenta Pandas com DataFrame, conhecimentos de testes de software e orientação a objetos. O objetivo é obter um código limpo, de fácil manutenção e legibilidade, melhorando a gestão das receitas e estoque do restaurante.",
